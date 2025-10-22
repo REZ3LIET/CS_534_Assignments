f6aa89c9282b7de7a858f77fa5d4d1a10252318d
REZ3LIET <rz.samar.kale@gmail.com>
2025-10-22 01:39:49 +0000

    BSAS implemented

--- Assignment_2/bsas.py
+++ Assignment_2/bsas.py
@@ -1,6 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class BSAS:
     def __init__(self) -> None:
-        pass
+        self.clusters = {}
 
     def load_data(self, data_path: str) -> tuple:
         """Loads the data from given text file and converts to tuples"""
@@ -18,9 +22,38 @@ class BSAS:
         # Return data as tuple
         return tuple(data)
     
-    def find_cluster(self, element) -> str:
+    def get_euclidean(self, ele_a: tuple, ele_b: tuple) -> float:
+        """Calculates the euclidean distance of given elements"""
+        x_ = (ele_a[0] - ele_b[0]) ** 2
+        y_ = (ele_a[1] - ele_b[1]) ** 2
+        return (x_ + y_) ** 0.5
+
+    def update_representative(self, cluster: str, element: tuple) -> tuple:
+        """Finds the new mean centroid of the given cluster"""
+        n_c = len(self.clusters[cluster][0])
+        m_c = self.clusters[cluster][-1]
+
+        # Calculating new centroid axis-wise
+        nm_x = ((n_c * m_c[0]) + element[0])/(n_c+1)
+        nm_y = ((n_c * m_c[1]) + element[1])/(n_c+1)
+        return (nm_x, nm_y)
+
+    def find_cluster(self, element: tuple) -> str:
         """Finds the cluster representative with closest to given element"""
+        tmp_dist = np.inf
         cluster_name = ""
+
+        # Loop to find closest cluster to element
+        for cluster in self.clusters.keys():
+            # Get representative
+            c_j = self.clusters[cluster][-1]
+
+            # Euclidean distance between element and cluster representative
+            dist = self.get_euclidean(element, c_j)
+            if dist < tmp_dist:
+                tmp_dist = dist
+                cluster_name = cluster
+
         return cluster_name
     
     def bsas_algorithm(self, data: tuple, q: int, theta: float) -> dict:
@@ -35,15 +68,48 @@ class BSAS:
         Returns:
             dict: dictionary of all the formulated clusters
         """
-        clusters = {}
-        return clusters
+        m = 1  # Initial cluster
+        self.clusters = {  # Collection of clusters
+            "C_1": [[data[0]], data[0]]  # cluster id: (data list, representative)
+        }
+
+        for i in range(2, len(data)+1):
+            x_i = data[i-1]
+            c_k = self.find_cluster(x_i)  # Get closest cluster to x_i
+
+            # Distance between cluster rep and x_i
+            dist = self.get_euclidean(x_i, self.clusters[c_k][-1])
+
+            # Validity check
+            if dist > theta and m < q:
+                m += 1
+                self.clusters[f"C_{m}"] = [[x_i], x_i]
+            else:
+                self.clusters[c_k][0].append(x_i)
+                self.clusters[c_k][-1] = self.update_representative(c_k, x_i)
+
+        return self.clusters
     
     def plot_clusters(self, clusters: dict) -> None:
         """Plots the given clusters"""
-        pass
+        plt.figure(figsize=(8, 6))
+
+        for label, (points, _) in clusters.items():
+            xs = [p[0] for p in points]
+            ys = [p[1] for p in points]
+            plt.scatter(xs, ys, label=label)  # Plot with label
+
+
+        plt.title("BSAS Cluster")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
 if __name__ == "__main__":
     clusterer = BSAS()
     data_path = "/workspaces/CS_534/CS_534_Assignments/Assignment_2/Data/cluster_data.txt"
     data = clusterer.load_data(data_path=data_path)
-    print(data[-5:])
+    clusters = clusterer.bsas_algorithm(data, 20, 0.5)
+    clusterer.plot_clusters(clusters)
